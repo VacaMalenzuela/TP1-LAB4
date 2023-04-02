fa95570fa77b6ebf0c5462c28a473e1130dae41d
Tomas Cañón <72145064+TomasNiCanon@users.noreply.github.com>
2023-04-02 18:55:49 -0300

    Update Principal.java
    subo el main

--- src/Ejercicio1/Principal.java
+++ src/Ejercicio1/Principal.java
@@ -1,10 +1,23 @@
 package Ejercicio1;
 
+import Ejercicio1.Empleado;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		   Empleado[]  empleados = new Empleado[5];
+	       
+	       empleados[0]= new Empleado();
+	       empleados[1]= new Empleado("Roberto",50);
+	       empleados[2]= new Empleado("Juan",35);
+	       empleados[3]= new Empleado();
+	       empleados[4]= new Empleado("Agustin",25);
+	       
+	       
+	       for (Empleado empleado : empleados) {
+			System.out.println(empleado.toString());
 	}
 
+}
 }
